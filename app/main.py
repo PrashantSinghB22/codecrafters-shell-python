64a4e07beb3614dd6c10c73f0dc640d356191294
Prashant Singh Balyan <235067837+PrashantSinghB22@users.noreply.github.com>
2026-07-25 14:00:00 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,6 +6,10 @@ def main():
         sys.stdout.write("$ ")
         # Wait for user input
         command = input()
+        if command == "exit":
+            break
         print(f"{command}: command not found")
+
+    
 if __name__ == "__main__":
     main()
