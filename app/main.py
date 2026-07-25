8a90f87e37efea4d605173f98f512c3dce89a6ef
Prashant Singh Balyan <235067837+PrashantSinghB22@users.noreply.github.com>
2026-07-25 15:27:33 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,76 +1,64 @@
 import sys
-import collections.abc import Callable
+from collections.abc import Callable
 import shutil
 
 Builtin = Callable[[list[str]], bool]
 
 BUILTIN = ["echo", "exit", "type"]
 
+
 def echo(arguments: list[str]) -> bool:
-  print(" ".join(arguments))
-  return True
-
-def exit_shell(_: list[str]) -> bool:
-  return False
-
-def type_command(arguments: list[str]) -> bool:
-  if not arguments:
-    print("type: missing argument")
+    print(" ".join(arguments))
     return True
 
-  command = arguments[0]
-  if command in BUILTIN:
-    print(f"{command} is a shell builtin")
 
-  elif executable := shutil.which(command):
-    print(f"{command} is {executable}")
+def exit_shell(_: list[str]) -> bool:
+    return False
 
-  else:
-    print(f"{command}: not found")
 
-  return True      
+def type_command(arguments: list[str]) -> bool:
+    if not arguments:
+        print("type: missing argument")
+        return True
+
+    command = arguments[0]
+    if command in BUILTIN:
+        print(f"{command} is a shell builtin")
+    elif executable := shutil.which(command):
+        print(f"{command} is {executable}")
+    else:
+        print(f"{command}: not found")
+    return True
+
 
 def main():
-  while True :
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
 
-    try: 
-      line = input()
-    except EOFError:
-      break
+        try:
+            line = input()
+        except EOFError:
+            break
 
-    if not line:
-      continue  
+        if not line:
+            continue
 
-    parts = line.split()
-    command = parts[0]
-    arguments = parts[1:]
+        parts = line.split()
+        command = parts[0]
+        arguments = parts[1:]
 
-    if command == "exit":
-      break
+        if command == "exit":
+            break
+        elif command == "echo":
+            echo(arguments)
+        elif command == "type":
+            type_command(arguments)
+        elif executable := shutil.which(command):
+            print(f"{command} is {executable}")
+        else:
+            print(f"{command}: command not found")
 
-    elif command == "echo":
-      echo(arguments)
-
-    elif command == "type":
-      type_command(arguments)
-
-    elif executable := shutil.which(command):
-      print(f"{command} is {executable}")
-
-    else:
-      print(f"{command}: command not found")
 
 if __name__ == "__main__":
-  main()
-
-
-  
-
-
-
-
-  
-  
-
+    main()
