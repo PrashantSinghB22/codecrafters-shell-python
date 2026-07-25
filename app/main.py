5fb4b73bcf40291993c23ef88855aa852217d59b
Prashant Singh Balyan <235067837+PrashantSinghB22@users.noreply.github.com>
2026-07-25 14:23:08 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,19 +1,36 @@
 import sys
 
 
+COMMANDS = {
+    "exit": lambda _: sys.exit(),
+    "echo": lambda args: print(" ".join(args)),
+    "type": lambda args: print(f"{args[0]} is a shell builtin")
+    if args[0] in COMMANDS
+    else print(f"{args[0]}: not found"),
+}
+
+
+def execute(cmd, *args):
+    if cmd in COMMANDS:
+        COMMANDS[cmd](*args)
+    else:
+        sys.stdout.write(f"{cmd}: command not found\n")
+
+
 def main():
     while True:
         sys.stdout.write("$ ")
-        # Wait for user input
-        command = input()
-        if command == "exit":
-            break
-        elif command.startswith("echo "):
-            print(command[5:])
-        else:
-            print(f"{command}: command not found")
+        sys.stdout.flush()
+        line = sys.stdin.readline()
+        line.strip()
+
+        if not line:
+            continue
+
+        cmd, *args = line.split()
+
+        execute(cmd, args)
 
 
-    
 if __name__ == "__main__":
     main()
