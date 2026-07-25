3c859ab9e50beda79d0e32c0d8ee79696654ba1b
Prashant Singh Balyan <235067837+PrashantSinghB22@users.noreply.github.com>
2026-07-25 15:26:41 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,36 +1,76 @@
 import sys
+import collections.abc import Callable
+import shutil
 
+Builtin = Callable[[list[str]], bool]
 
-COMMANDS = {
-    "exit": lambda _: sys.exit(),
-    "echo": lambda args: print(" ".join(args)),
-    "type": lambda args: print(f"{args[0]} is a shell builtin")
-    if args[0] in COMMANDS
-    else print(f"{args[0]}: not found"),
-}
+BUILTIN = ["echo", "exit", "type"]
 
+def echo(arguments: list[str]) -> bool:
+  print(" ".join(arguments))
+  return True
 
-def execute(cmd, *args):
-    if cmd in COMMANDS:
-        COMMANDS[cmd](*args)
-    else:
-        sys.stdout.write(f"{cmd}: command not found\n")
+def exit_shell(_: list[str]) -> bool:
+  return False
 
+def type_command(arguments: list[str]) -> bool:
+  if not arguments:
+    print("type: missing argument")
+    return True
+
+  command = arguments[0]
+  if command in BUILTIN:
+    print(f"{command} is a shell builtin")
+
+  elif executable := shutil.which(command):
+    print(f"{command} is {executable}")
+
+  else:
+    print(f"{command}: not found")
+
+  return True      
 
 def main():
-    while True:
-        sys.stdout.write("$ ")
-        sys.stdout.flush()
-        line = sys.stdin.readline()
-        line.strip()
+  while True :
+    sys.stdout.write("$ ")
+    sys.stdout.flush()
 
-        if not line:
-            continue
+    try: 
+      line = input()
+    except EOFError:
+      break
 
-        cmd, *args = line.split()
+    if not line:
+      continue  
 
-        execute(cmd, args)
+    parts = line.split()
+    command = parts[0]
+    arguments = parts[1:]
 
+    if command == "exit":
+      break
+
+    elif command == "echo":
+      echo(arguments)
+
+    elif command == "type":
+      type_command(arguments)
+
+    elif executable := shutil.which(command):
+      print(f"{command} is {executable}")
+
+    else:
+      print(f"{command}: command not found")
 
 if __name__ == "__main__":
-    main()
+  main()
+
+
+  
+
+
+
+
+  
+  
+
