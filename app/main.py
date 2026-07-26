b27d3358e1ebc2ad6e3e9a60bb984530f3688960
Prashant Singh Balyan <235067837+PrashantSinghB22@users.noreply.github.com>
2026-07-26 12:53:49 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,63 +1,74 @@
 import sys
-from collections.abc import Callable
-import shutil
-
-Builtin = Callable[[list[str]], bool]
-
-BUILTIN = ["echo", "exit", "type"]
+import os
+import subprocess
+from typing import Union
 
 
-def echo(arguments: list[str]) -> bool:
-    print(" ".join(arguments))
-    return True
+class Shell(object):
+    """Basic Shell class"""
 
+    __return_code: int = 0
+    __path: list[str] = []
+    __METHODS: list[str] = []
 
-def exit_shell(_: list[str]) -> bool:
-    return False
+    def __init__(self) -> None:
+        super().__init__()
+        self.__path = os.environ["PATH"].split(":")
+        self.__METHODS = [
+            f
+            for f in dir(self)
+            if callable(getattr(self, f)) and "__" not in f and not f.startswith("_")
+        ]  # pyright: ignore[reportConstantRedefinition, reportAny, reportAttributeAccessIssue, reportUnknownMemberType]
+        self.METHODS: list = self.__METHODS
 
+    def exit(self) -> None:
+        sys.exit(0)
 
-def type_command(arguments: list[str]) -> bool:
-    if not arguments:
-        print("type: missing argument")
-        return True
+    def echo(self, *args: list[str]) -> None:
+        print(f"{' '.join(args)}")  # pyright: ignore[reportCallIssue, reportArgumentType]
+        self.__return_code = 0
 
-    command = arguments[0]
-    if command in BUILTIN:
-        print(f"{command} is a shell builtin")
-    elif executable := shutil.which(command):
-        print(f"{command} is {executable}")
-    else:
-        print(f"{command}: not found")
-    return True
+    def __walk_path(self, command: str) -> str:
+        for p in self.__path:
+            path: str = os.path.join(p, command)
+            if os.path.exists(path) and os.access(path, os.X_OK):
+                return path
+        return ""
+
+    def _valid_command(self, command: str) -> function | str | None:
+        if command in self.__METHODS:
+            return getattr(self, command)
+        elif exe := self.__walk_path(command):
+            return exe
+        else:
+            return None
+
+    def type(self, *args) -> None:  # pyright: ignore[reportUnknownParameterType, reportMissingParameterType]
+        command: str = args[0]  # pyright: ignore[reportUnknownVariableType]
+        targ = self._valid_command(command)
+        if callable(targ):
+            print(f"{command} is a shell builtin")
+        elif targ:
+            print(f"{command} is {targ}")
+        else:
+            print(f"{command}: not found")
+            self.__return_code = 0
 
 
 def main():
+    my_shell = Shell()
     while True:
-        sys.stdout.write("$ ")
-        sys.stdout.flush()
+        _ = sys.stdout.write("$ ")
+        usr_input = input()
+        first = usr_input.split()[0]
+        if run := my_shell._valid_command(first):
+            if callable(run):
+                run(*usr_input.split()[1:])
+            else:
+                print(subprocess.check_output(usr_input.split()).decode(), end="")
 
-        try:
-            line = input()
-        except EOFError:
-            break
-
-        if not line:
-            continue
-
-        parts = line.split()
-        command = parts[0]
-        arguments = parts[1:]
-
-        if command == "exit":
-            break
-        elif command == "echo":
-            echo(arguments)
-        elif command == "type":
-            type_command(arguments)
-        elif executable := shutil.which(command):
-            print(f"{command} is {executable}")
         else:
-            print(f"{command}: command not found")
+            print(f"{first}: command not found")
 
 
 if __name__ == "__main__":
